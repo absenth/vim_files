85ce4871c604e320538b2eb6a8c9f58713bbbc68
Lars Noldan <Lars.Noldan@care.com>
2021-04-16 08:43:15 -0500

    configure yamllint and editorconfig and expand setup script

--- setup.bash
+++ setup.bash
@@ -2,8 +2,12 @@
 
 mkdir -p $HOME/.vim/plugin
 mkdir -p $HOME/.vim/after/plugin
+mkdir -p $HOME/.config/yamllint
 
 for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
     rm -rf ~/.vim/$f
     ln -s $(PWD)/$f ~/.vim/$f
 done
+
+ln -s $(PWD)/editorconfig $HOME/.editorconfig
+ln -s $(PWD)/yamllint-conf $HOME/.config/yamllint/config
