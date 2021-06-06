60507205d33b1502a3052a3a72c04ebf5e663b4a
absenth <wintermute223@gmail.com>
2021-06-06 18:33:36 -0500

    Fixed setup.bash and updated README

--- README.md
+++ README.md
@@ -4,7 +4,5 @@
 
 1. Clone This Repo
 2. Run /bin/bash setup.bash
-3. Fix everything the setup.bash didn't do
-4. In fact just don't run the setup script.
 5. [Install yamllint](https://yamllint.readthedocs.io/en/stable/quickstart.html#installing-yamllint)
 6. Thank [ThePrimeagen](https://github.com/ThePrimeagen/)
--- setup.bash
+++ setup.bash
@@ -4,11 +4,7 @@ mkdir -p $HOME/.vim/plugin
 mkdir -p $HOME/.vim/after/plugin
 mkdir -p $HOME/.config/yamllint
 
-for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
-    rm -rf ~/.vim/$f
-    ln -s $(PWD)/$f ~/.vim/$f
-done
-
-ln -s $(PWD)/configs/editorconfig $HOME/.editorconfig
-ln -s $(PWD)/configs/yamllint-conf $HOME/.config/yamllint/config
-ln -s $(PWD)/vimrc $HOME/.vimrc
+ln -s $(pwd)/configs/editorconfig $HOME/.editorconfig
+ln -s $(pwd)/configs/yamllint-conf $HOME/.config/yamllint/config
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/vim $HOME/.vim
